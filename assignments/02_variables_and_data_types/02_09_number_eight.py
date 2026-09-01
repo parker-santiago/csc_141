dc86f2e9f52530189af9157f152fe809254cf34a
parker-santiago <parker.santiago001@albright.edu>
2026-09-01 12:18:36 -0400

    commit 3

--- assignments/02_variables_and_data_types/02_09_number_eight.py
+++ assignments/02_variables_and_data_types/02_09_number_eight.py
@@ -1,1 +1,5 @@
-# new
+# Parker Santiago
+print(5 + 3)
+print(12 - 4)
+print(4 * 2)
+print(32 / 4)
---
+++ assignments/02_variables_and_data_types/02_10_favorite_number.py
@@ -0,0 +1,2 @@
+favorite_number = ((2 + 8) * 1.5) - 8
+print(f"My favorite number is {int(favorite_number)}.")
