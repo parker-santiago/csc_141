f46971e3f847bffb34f64b88520419bf1ae70a74
parker-santiago <parker.santiago001@albright.edu>
2026-09-15 11:29:23 -0400

    7th commit

--- assignments/03_introducing_lists/03_10_every_function.py
+++ assignments/03_introducing_lists/03_10_every_function.py
@@ -7,9 +7,25 @@ print(f"I need to buy {len(groceries)} items from the store.")
 print(f"\nThe first item on my list is {groceries[0]}.")
 print(f"The last item on my list is {groceries[-1]}.")
 
-
+# title & upper
 print(f"\n{groceries[0].title()} is the most important item on my list.")
 print(f"We had some earlier but then i SPILLED THE {groceries[0].upper()} ALL OVER THE DAMN FLOOR. I need to buy more.")
 
-
+# pop & del
 print(f"\nAlright, just picked up some {groceries.pop(0)}, only {len(groceries)} more to go")
+print(f"Going to pick up some {groceries[0]} next!")
+del groceries[0]
+
+# insert & append
+groceries.append("chocolate")
+groceries.insert(0, "cereal")
+
+print(f"\nI just added {groceries[-1]} to my list, and I also added {groceries[0]} to the beginning of my list.")
+
+# sort & reverse
+print(groceries)
+print(sorted(groceries))
+groceries.reverse()
+print(groceries)
+groceries.reverse()
+print(groceries)
---
+++ assignments/03_introducing_lists/03_11_intentional_error.py
@@ -0,0 +1,12 @@
+# Parker Santiago
+
+friends = ["talan", "emery", "quan", "james", "bryce"]
+# Actual list of my good friends!
+print(friends[1].title())
+print(friends[2].title())
+print(friends[3].title())
+print(friends[4].title())
+print(friends[5].title())
+
+# In this, the last line will throw an error because index 5 does not exist inside the list.
+# I should actually start at index 0 and go to index 4
